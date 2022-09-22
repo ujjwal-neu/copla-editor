d0764dc3fc05c14e6fd9d871cb01645ab9f56d9e
Shoaibahmed997-2 <shoiab.ahmed@neuphony.com>
2022-09-22 14:08:55 +0530

    feat: sending annotation and marker data

--- sn-editor/src/components/Graph.js
+++ sn-editor/src/components/Graph.js
@@ -21,6 +21,8 @@ export default class Graph extends Component {
     onChange: PropTypes.func.isRequired,
     minRange: PropTypes.number,
     maxRange: PropTypes.number,
+    markerData:PropTypes.array,
+    annotationData:PropTypes.array
   }
 
   static defaultProps = {
@@ -30,14 +32,18 @@ export default class Graph extends Component {
     artifacts: null,
   }
 
+
   minHeight = 60
 
   componentDidMount() {
     if (this.container) this.createGraph();
+    console.log('markerData from graph component',this.props.markerData)
+    console.log('annotation from graph component',this.props.annotationData)
   }
 
   componentDidUpdate(prevProps) {
     if (!this.graph) return;
+ 
 
     if (this.props.dateWindow !== prevProps.dateWindow) {
       this.graph.dateWindow_ = this.props.dateWindow;
@@ -65,6 +71,7 @@ export default class Graph extends Component {
 
   getOptions = () => {
     const { channel, dateWindow, height, minRange, maxRange } = this.props;
+    console.log('anno data: ',this.props.annotationData)
     return {
       dateWindow,
       // TODO toggle dynamic range / physical range ???
@@ -146,7 +153,6 @@ export default class Graph extends Component {
     const options = this.getOptions();
     const value = [dateWindow[0], [channel.physicalMinimum, 0, channel.physicalMaximum]];
     const graph = new Dygraph(this.container, [value], options);
-    console.log(graph)
     // graph.setAnnotations([
     //   {
     //     series: channel.standardLabel,
